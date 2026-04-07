9c5178e42b14a6e9b94d5812a10ad4d1cbb515c8
Andrey Podovinov <podovinov2003andrey@gmail.com>
2026-04-08 04:03:32 +0600

    fix

--- fashion_clip/fashion_clip.py
+++ fashion_clip/fashion_clip.py
@@ -156,8 +156,8 @@ class FashionCLIP:
         if name in _MODELS or _is_hugging_face_repo(name, auth_token):
             # if using know short-hand, extract from dict
             name = _MODELS[name] if name in _MODELS else name
-            model = CLIPModel.from_pretrained(name, use_auth_token=auth_token)
-            preprocessing = CLIPProcessor.from_pretrained(name, use_auth_token=auth_token)
+            model = CLIPModel.from_pretrained(name)
+            preprocessing = CLIPProcessor.from_pretrained(name)
             hash = _model_processor_hash(name, model, preprocessing)
 
         # else it doesn't use HF, assume using OpenAI CLiP
